d867808945e7e6e9a849ee6552b6f728761eaa21
filipe satiro <filipelipe202229@gmail.com>
2025-06-27 08:07:31 -0300

    criada a pasta projeto em javascript

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,8 +1,19 @@
+import Input from '../form/Input'
+import styles from './ProjectForm.module.css'
 function ProjectForm() {
     return (
-        <form>
-            <div><input type="text" placeholder="Insira o nome do projeto" /></div>
-            <div><input type="number" placeholder="Insira o orçamento Total" /></div>
+        <form className={styles.form}>
+            <Input type="text"
+            text="Nome do projeto"
+            name="name"
+            placeholder="Insira o nome do projeto" />
+
+           
+            <Input type="number"
+            text="Orçamento do projeto"
+            name="budget"
+            placeholder="Insira o orçamento total" />
+
             <div>
             <select name="category_id">
             <option disabled selected >Selecione a Categoria</option>
