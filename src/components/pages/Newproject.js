055e36d3669488754628388b1b15a19592414893
filipe satiro <filipelipe202229@gmail.com>
2025-06-27 09:05:54 -0300

    uma pequena alteração na pasta pai

--- src/components/pages/Newproject.js
+++ src/components/pages/Newproject.js
@@ -5,7 +5,7 @@ function Newproject() {
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu Projeto para depois adicionar os serviços</p>
-            <ProjectForm/>
+            <ProjectForm btnText="Criar Projeto"/>
         </div>
     )
 
