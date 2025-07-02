50749ee1f79504e3fb9542718d2a7f0de22ecf35
filipe satiro <filipelipe202229@gmail.com>
2025-07-02 12:49:44 -0300

    incrementando o Project.js para deixar o formulário mais dinâmico

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -12,8 +12,9 @@ function Project () {
         const {id} = useParams()
         const [project, setProject] = useState({})
         const [showProjectForm, setShowProjectForm] = useState(false)
-        const [message, setMessage] = useState()
-        const [type, setType] = useState()
+        const [showServiceForm, setShowServiceForm] = useState(false)
+        const [message, setMessage] = useState('')
+        const [type, setType] = useState('')
 
 
         useEffect(() => {
@@ -33,6 +34,7 @@ function Project () {
         }, [id])
 
         function editPost(project){
+            setMessage('')
             //budget validation
             if (project.budget < project.cost) {
                 setMessage('O orçamento não pode ser menor que o custo do Projeto!')
@@ -60,6 +62,9 @@ function Project () {
         function toggleProjectForm() {
             setShowProjectForm(!showProjectForm)
         }
+           function toggleServiceForm() {
+            setShowServiceForm(!showServiceForm)
+        }
 
     return (
     
@@ -69,31 +74,41 @@ function Project () {
          <div className={styles.project_details}>
             <Container customClass="column"> 
                 {message && <Message type={type} msg={message} />}
-                <div className={styles.details_container}>
-                    <h1>Projeto: {project.name}</h1>
-                    <button className={styles.btn} onClick={toggleProjectForm}>
-                        {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
-                        </button>
-                        {!showProjectForm ? (
-                            <div className={styles.project_info}>
-                                <p><span>Categoria:</span> {project.category.name}</p>
-                                <p><span>Total de Orçamento:</span> R$ {project.budget}</p>
-                                <p><span>Total utilizado:</span> R$ {project.cost}</p>
-                            </div>
-                        ) : (
-                            <div className={styles.project_info}>
-                                <ProjectForm handleSubmit={editPost} btnText="Concluir edição" projectData={project} />
-                            </div>
-                        )}
+    <div className={styles.details_container}>
+        <h1>Projeto: {project.name}</h1>
+        <button className={styles.btn} onClick={toggleProjectForm}>
+        {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
+        </button>
+        {!showProjectForm ? (
+            <div className={styles.project_info}>
+                <p><span>Categoria:</span> {project.category.name}</p>
+                <p><span>Total de Orçamento:</span> R$ {project.budget}</p>
+                <p><span>Total utilizado:</span> R$ {project.cost}</p>
+            </div>
+        ) : (
+            <div className={styles.project_info}>
+                <ProjectForm handleSubmit={editPost} btnText="Concluir edição" projectData={project} />
+            </div>
+            )}   
                 </div>
+                  <div className={styles.service_form_container}>
+                    <h2>Adicione um serviço: </h2>
+                    <button className={styles.btn} onClick={toggleServiceForm}>
+                        {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+                        
+        </button>
+        </div>
+            <div className={styles.project_info}>
+            {showServiceForm && (<div>Formulário do Serviço</div>)}
+            </div>  
+            <h2>Serviços</h2>
+            <Container customClass="start">
+            <p>Itens de Serviços</p>
             </Container>
-         </div>
-          ): (
-          <Loading />
-          )}
-          
-          </>
-    )
-}
+            </Container>
+            </div>
+            ): (<Loading />)}
+            </>
+            )}
 
 export default Project
